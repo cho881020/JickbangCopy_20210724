38eefafa75ccf01d46067b8d6c3d686fb3da50b6
조경진 <cho881020@gmail.com>
2021-07-24 10:47:34 +0900

    방을 뿌려주는 Adapter 클래스 생성 부분만 작성.

---
+++ app/src/main/java/com/neppplus/jickbangcopy_20210724/adapters/RoomAdapter.kt
@@ -0,0 +1,11 @@
+package com.neppplus.jickbangcopy_20210724.adapters
+
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20210724.datas.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList : ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+}
